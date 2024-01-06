8a7c412fb1777acb3980a4382e03da347c44ca2e
veroarriola <43796217+veroarriola@users.noreply.github.com>
2024-01-06 00:41:31 -0600

    Ya funciona el diálogo para cambiar la IP.

--- app/src/main/java/mx/unam/fciencias/controlremoto/MainActivity.kt
+++ app/src/main/java/mx/unam/fciencias/controlremoto/MainActivity.kt
@@ -6,11 +6,16 @@ import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.Icon
@@ -19,11 +24,22 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.window.Dialog
 import mx.unam.fciencias.controlremoto.ui.theme.ControlRemotoTheme
 
 class MainActivity : ComponentActivity() {
@@ -48,6 +64,9 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Steering(conModel: ConnectionsModel, modifier: Modifier = Modifier) {
+    var showIpDialog = remember { mutableStateOf(false) }
+    var context = LocalContext.current
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -57,7 +76,7 @@ fun Steering(conModel: ConnectionsModel, modifier: Modifier = Modifier) {
                 actions = {
                     IconButton(
                         onClick = {
-                        /* do something */
+                            showIpDialog.value = true
                         }
                     ) {
                         Icon(
@@ -75,6 +94,75 @@ fun Steering(conModel: ConnectionsModel, modifier: Modifier = Modifier) {
                 text = "Hello in ${conModel.piURL}",
                 modifier = modifier
             )
+        }
+        if (showIpDialog.value) {
+            IPDialogForm(
+                conModel.piURL,
+                onDismissRequest = {
+                    showIpDialog.value = false
+                },
+                onConfirmation = { link : String ->
+                    conModel.piURL = link
+                    showIpDialog.value = false
+                }
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun IPDialogForm(
+    currentIP: String,
+    onDismissRequest: () -> Unit,
+    onConfirmation: (link : String) -> Unit
+) {
+    var ipText by rememberSaveable(stateSaver = TextFieldValue.Saver) {
+        mutableStateOf(TextFieldValue(currentIP))
+    }
+    Dialog(onDismissRequest = { onDismissRequest() }) {
+        Card {
+            Column (
+                verticalArrangement = Arrangement.Center
+            ) {
+                TextField(
+                    value = ipText,
+                    onValueChange = {
+                        it.also { ipText = it }
+                    }
+                )
+                AcceptCancelRow(
+                    onDismissRequest = { onDismissRequest() },
+                    onConfirmation = {
+                        onConfirmation(ipText.text)
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun AcceptCancelRow(
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center,
+    ) {
+        TextButton(
+            onClick = { onDismissRequest() },
+            modifier = Modifier.padding(8.dp),
+        ) {
+            Text("Cancelar")
+        }
+        TextButton(
+            onClick = { onConfirmation() },
+            modifier = Modifier.padding(8.dp),
+        ) {
+            Text("Aceptar")
         }
     }
 }
