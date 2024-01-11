df9d7ff4bb4388a9a8b9a7e87fd77b24a3305283
Vero <v.arriola@ciencias.unam.mx>
2024-01-11 00:01:11 -0600

    ¡Ya funciona!

--- app/src/main/java/mx/unam/fciencias/controlremoto/SteeringControl.kt
+++ app/src/main/java/mx/unam/fciencias/controlremoto/SteeringControl.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -17,10 +18,12 @@ import androidx.constraintlayout.compose.ConstraintLayout
 import mx.unam.fciencias.controlremoto.ui.theme.ControlRemotoTheme
 import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
+import java.io.IOException
 import java.io.InputStream
 import java.io.OutputStream
 import java.net.HttpURLConnection
 import java.net.NoRouteToHostException
+import java.net.ProtocolException
 import java.net.URL
 
 
@@ -37,11 +40,12 @@ fun SteeringControl(conModel: ConnectionsModel, modifier: Modifier = Modifier) {
         val endGuideline = createGuidelineFromEnd(4.dp)
         val startGuideline = createGuidelineFromStart(4.dp)
 
-        var showToast = remember { mutableStateOf(false) }
         var toastText = remember { mutableStateOf("") }
 
         ComposeImageButton(
-            onClick = { /* Do something */ },
+            onClick = { /* Do something */
+                postCommand(conModel = conModel, command = "SE", toastText)
+            },
             buttonModifier = Modifier.constrainAs(buttonSE) {
                 bottom.linkTo(bottomGuideline)
                 end.linkTo(endGuideline)
@@ -54,7 +58,7 @@ fun SteeringControl(conModel: ConnectionsModel, modifier: Modifier = Modifier) {
                 }
         )
         ComposeImageButton(
-            onClick = { /* Do something */ },
+            onClick = { postCommand(conModel = conModel, command = "backward", toastText) },
             buttonModifier = Modifier.constrainAs(buttonBackward) {
                 end.linkTo(buttonSE.start)
                 bottom.linkTo(bottomGuideline)
@@ -67,7 +71,7 @@ fun SteeringControl(conModel: ConnectionsModel, modifier: Modifier = Modifier) {
                 }
         )
         ComposeImageButton(
-            onClick = { /* Do something */ },
+            onClick = { postCommand(conModel = conModel, command = "SW", toastText) },
             buttonModifier = Modifier.constrainAs(buttonSW) {
                 end.linkTo(buttonBackward.start)
                 bottom.linkTo(bottomGuideline)
@@ -80,7 +84,7 @@ fun SteeringControl(conModel: ConnectionsModel, modifier: Modifier = Modifier) {
                 }
         )
         ComposeImageButton(
-            onClick = { /* Do something */ },
+            onClick = { postCommand(conModel = conModel, command = "right", toastText) },
             buttonModifier = Modifier.constrainAs(buttonRight) {
                 bottom.linkTo(buttonSE.top)
                 end.linkTo(endGuideline)
@@ -93,7 +97,7 @@ fun SteeringControl(conModel: ConnectionsModel, modifier: Modifier = Modifier) {
                 }
         )
         ComposeImageButton(
-            onClick = { /* Do something */ },
+            onClick = { postCommand(conModel = conModel, command = "stop", toastText) },
             buttonModifier = Modifier.constrainAs(buttonStop) {
                 bottom.linkTo(buttonBackward.top)
                 end.linkTo(buttonRight.start)
@@ -103,7 +107,7 @@ fun SteeringControl(conModel: ConnectionsModel, modifier: Modifier = Modifier) {
             imageModifier = Modifier
         )
         ComposeImageButton(
-            onClick = { /* Do something */ },
+            onClick = { postCommand(conModel = conModel, command = "left", toastText) },
             buttonModifier = Modifier.constrainAs(buttonLeft) {
                 bottom.linkTo(buttonSW.top)
                 end.linkTo(buttonStop.start)
@@ -116,7 +120,7 @@ fun SteeringControl(conModel: ConnectionsModel, modifier: Modifier = Modifier) {
                 }
         )
         ComposeImageButton(
-            onClick = { /* Do something */ },
+            onClick = { postCommand(conModel = conModel, command = "NE", toastText) },
             buttonModifier = Modifier.constrainAs(buttonNE) {
                 bottom.linkTo(buttonRight.top)
                 end.linkTo(endGuideline)
@@ -131,9 +135,7 @@ fun SteeringControl(conModel: ConnectionsModel, modifier: Modifier = Modifier) {
         ComposeImageButton(
             onClick =
             { /* Do something */
-                postCommand(conModel = conModel, command = "forward")
-                toastText.value = "Comando forward enviado"
-                showToast.value = true
+                postCommand(conModel = conModel, command = "forward", toastText)
             },
             buttonModifier = Modifier.constrainAs(buttonForward) {
                 end.linkTo(buttonNE.start)
@@ -143,7 +145,7 @@ fun SteeringControl(conModel: ConnectionsModel, modifier: Modifier = Modifier) {
             contentDescription = stringResource(id = R.string.forward)
         )
         ComposeImageButton(
-            onClick = { /* Do something */ },
+            onClick = { postCommand(conModel = conModel, command = "NW", toastText) },
             buttonModifier = Modifier.constrainAs(buttonNW) {
                 end.linkTo(buttonForward.start)
                 bottom.linkTo(buttonStop.top)
@@ -156,7 +158,7 @@ fun SteeringControl(conModel: ConnectionsModel, modifier: Modifier = Modifier) {
                 }
         )
         ComposeImageButton(
-            onClick = { /* Do something */ },
+            onClick = { postCommand(conModel = conModel, command = "turn_right", toastText) },
             buttonModifier = Modifier.constrainAs(buttonTurnRight) {
                 bottom.linkTo(bottomGuideline)
                 start.linkTo(startGuideline)
@@ -165,7 +167,7 @@ fun SteeringControl(conModel: ConnectionsModel, modifier: Modifier = Modifier) {
             contentDescription = stringResource(id = R.string.turn_right)
         )
         ComposeImageButton(
-            onClick = { /* Do something */ },
+            onClick = { postCommand(conModel = conModel, command = "turn_left", toastText) },
             buttonModifier = Modifier.constrainAs(buttonTurnLeft) {
                 bottom.linkTo(buttonTurnRight.top)
                 start.linkTo(startGuideline)
@@ -174,7 +176,7 @@ fun SteeringControl(conModel: ConnectionsModel, modifier: Modifier = Modifier) {
             contentDescription = stringResource(id = R.string.turn_left)
         )
         ComposeImageButton(
-            onClick = { /* Do something */ },
+            onClick = { postCommand(conModel = conModel, command = "brake", toastText) },
             buttonModifier = Modifier.constrainAs(buttonBrake) {
                 bottom.linkTo(buttonTurnLeft.top)
                 start.linkTo(startGuideline)
@@ -183,7 +185,7 @@ fun SteeringControl(conModel: ConnectionsModel, modifier: Modifier = Modifier) {
             contentDescription = stringResource(id = R.string.brake)
         )
         ComposeImageButton(
-            onClick = { /* Do something */ },
+            onClick = { postCommand(conModel = conModel, command = "accelerate", toastText) },
             buttonModifier = Modifier.constrainAs(buttonAccelerate) {
                 bottom.linkTo(buttonBrake.top)
                 start.linkTo(startGuideline)
@@ -191,41 +193,53 @@ fun SteeringControl(conModel: ConnectionsModel, modifier: Modifier = Modifier) {
             imageVector = ImageVector.vectorResource(id = R.drawable.acelerar),
             contentDescription = stringResource(id = R.string.accelerate)
         )
-        if(showToast.value) {
+        if(toastText.value.isNotEmpty()) {
             Toast.makeText(LocalContext.current,
                 toastText.value,
                 Toast.LENGTH_SHORT).show()
-            showToast.value = false
+            toastText.value = ""
         }
     }
 }
 
-fun postCommand(conModel: ConnectionsModel, command: String) {
+fun postCommand(conModel: ConnectionsModel, command: String, toastText: MutableState<String>) {
     // https://developer.android.com/reference/java/net/HttpURLConnection
+    toastText.value = "Enviando comando ${command}"
     val thread = Thread {
         val url = URL(conModel.piURLCommand)
         val urlConnection = url.openConnection() as HttpURLConnection
         var respuesta: String
         try {
-            //urlConnection.doOutput = true
-            ////urlConnection.setChunkedStreamingMode(0)
-            //val bytesToSend = command.toByteArray()
-            //urlConnection.setFixedLengthStreamingMode(bytesToSend.size)
+            urlConnection.requestMethod = "POST"
+            urlConnection.addRequestProperty("command", command)
+            urlConnection.doOutput = true
 
-            //val out: OutputStream = BufferedOutputStream(urlConnection.outputStream)
-            //out.write(bytesToSend)
+            //urlConnection.setChunkedStreamingMode(0)
+            val bytesToSend = command.toByteArray()
+            urlConnection.setFixedLengthStreamingMode(bytesToSend.size)
+
+            val out: OutputStream = BufferedOutputStream(urlConnection.outputStream)
+            //out.write(command.toByteArray())
+            out.write(bytesToSend)
+            out.flush()
+            out.close()
 
             if(urlConnection.responseCode == 200) {
                 val `in`: InputStream = BufferedInputStream(urlConnection.inputStream)
                 //readStream(`in`)
-                /*Snackbar.make(view, `in`, Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()*/
                 //respuesta = `in`.toString()
+                toastText.value = `in`.toString()
             } else {
                 val err: InputStream = BufferedInputStream(urlConnection.errorStream)
+                toastText.value = "Conexión != 200"
             }
         } catch (e: NoRouteToHostException) {
             // ¿Cómo aviso que se conecte?
+            toastText.value = "Sin ruta al huesped."
+        } catch (e: ProtocolException) {
+            toastText.value = "Error $e."
+        } catch (e: IOException) {
+            toastText.value = "Flujo roto: $e."
         } finally {
             urlConnection.disconnect()
         }
